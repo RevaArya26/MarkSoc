e4efb6c1de02b34e6dcfeef5afd2899a5ae44fb2
RevaArya26 <revaarya2610@gmail.com>
2023-09-08 03:21:23 +0530

    Home Stats/ Speakers Slider

--- script_home.js
+++ script_home.js
@@ -392,3 +392,21 @@ var docWidth = document.documentElement.offsetWidth;
     }
   }
 );
+
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 1000;
+
+
+valueDisplays.forEach((valueDisplay) => {
+  let start = 0
+  let end = parseInt(valueDisplay.getAttribute("data-val"));
+
+  let duration = Math.floor(interval / end);
+  let counter = setInterval(function () {
+    start += 5;
+    valueDisplay.textContent = start;
+    if (start == end) {
+      clearInterval(counter)
+    }
+  }, duration);
+});
